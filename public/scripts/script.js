63c9f3cf661bb48d1a6ef076b9d7f07ce090a656
max44444444444 <pb31qbc@eduvaud.ch>
2024-01-23 16:50:37 +0100

    little optimisation playBoard

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,35 +7,33 @@ addEventListener("load", (event) => {
 });
 
 function createPlayBoard(){
-    for(let x = 0; x <= 8; x++){
+    for(let x = 0; x <= 7; x++){
         console.log("x")
         let column = document.createElement("div");
         chessBoard.appendChild(column);
-        for(let y = 0; y <= 8; y++){
+        for(let y = 0; y <= 7; y++){
             console.log("y")
             column.className = "column";
             if(x % 2 == 0){
                 if(y % 2 !== 0){
-                    let whiteCases = document.createElement("div");
-                    column.appendChild(whiteCases)
-                    whiteCases.className = 'whiteCase';
+                    createCase(column, "black");
                 }else{
-                    let blackCases = document.createElement("div");
-                    column.appendChild(blackCases);
-                    blackCases.className = 'blackCase';                
+                    createCase(column, "white");               
                 }
             }else{
                 if(y % 2 === 0){
-                    let whiteCases = document.createElement("div");
-                    column.appendChild(whiteCases)
-                    whiteCases.className = 'whiteCase';
+                    createCase(column, "black");
                 }else{
-                    let blackCases = document.createElement("div");
-                    column.appendChild(blackCases);
-                    blackCases.className = 'blackCase';                
+                    createCase(column, "white");             
                 }
             }
             
         }
     }
+}
+
+function createCase(column, color){
+    const oneCase = document.createElement("div");
+    column.appendChild(oneCase);
+    oneCase.className = color;
 }
