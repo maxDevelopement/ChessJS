72865313a977c8f27a6a9580cf363102e6000593
max44444444444 <pb31qbc@eduvaud.ch>
2024-01-29 13:35:07 +0100

    continuation de la construction des class

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,36 +1,28 @@
+let User = {
+    username: undefined,
+    color: undefined,
+    takenPieces: []
+};
 
 
 class Case{
-    constructor(xPosition, yPosition, grade = "none", caseColor = "none", playerColor = "none", htmlElement = "none"){
+    constructor(xPosition, yPosition, grade = "none", caseColor = "none", pieceColor = "none", htmlElement = "none"){
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this._grade = grade;
         this.caseColor = caseColor;
-        this.playerColor = playerColor;
+        this.pieceColor = pieceColor;
         this.htmlElement = htmlElement;
     } 
     get grade(){
         return this._grade;
     }
     // changement automatique de l'image lors de déplacement de pièces
-    set grade(newGrade){ 
+    set grade(grade){ 
+        let newGrade = new Grade(grade);
         this._grade = newGrade;
-        this.updateImg();
+        newGrade.updateImg();
     }
-
-    updateImg(){ 
-        console.log("update img !");
-        let blackPath = './img/Black_Chessman';
-        let whitePath = './img/White_Chessman';
-            let div = this.htmlElement;
-            switch(this._grade){
-                case "king": {
-                    console.log("grade : ", this._grade);
-                    div.style.backgroundImage = `url(${blackPath}/Black_King.png)`;
-            }
-        }
-    }
-
     createCase(line, color){
         const oneCase = document.createElement("div");
         line.appendChild(oneCase);
@@ -39,11 +31,13 @@ class Case{
         this.htmlElement = oneCase;
         this.htmlElement.addEventListener('click', this.handleClick);
     }
-    
+    // affiche automatiquement l'image de la pièce selon son grade
     handleClick= () => {
         console.log("objet : ", this.htmlElement);
         let div = this.htmlElement;
         if(activClick === false){ 
+            console.log(this);
+            this.grade.ShowMove(this.xPosition, this.yPosition);
             div.style.backgroundColor = "red";
             activClick = true;
         }else if(activClick === true){ // rajouter conditions pour checker si case clickée est dans les case des mouvements possible
@@ -51,9 +45,6 @@ class Case{
             activClick = false;
         }
     };
-    ShowMovement(){
-        console.log("movements possibles !")
-    }
     // remets toutes les cases du tableau de jeu dans leurs couleurs d'origine
     CleanMovements(){
         console.log("chessBoard : ", chessBoardArray);
@@ -67,6 +58,114 @@ class Case{
     }
 }
 
+class Grade{
+    constructor(name){
+        this.name = name;
+    }
+    updateImg(color){ 
+        console.log("update img !");
+        let path = `./img/${color}`;
+        let div = this.htmlElement;
+        switch(this._grade){
+            case "king": {
+                console.log("grade : ", this._grade);
+                if(this.pieceColor === "black"){
+                    div.style.backgroundImage = `url(${path}/Black_King.png)`;
+                }else if(this.pieceColor === "white"){
+                    div.style .backgroundImage = `url(${path}/White_King.png)`;
+                }         
+                break;
+            }
+            case "queen": {
+                if(this.pieceColor === "black"){
+                    div.style.backgroundImage = `url(${path}/Black_Queen.png)`;
+                }else if(this.caseColor === "white"){
+                    div.style .backgroundImage = `url(${path}/White_Queen.png)`;
+                }         
+                break;
+            }
+            case "bishop": { // fou
+                if(this.pieceColor === "black"){
+                    div.style.backgroundImage = `url(${path}/Black_Bishop.png)`;
+                }else if(this.pieceColor === "white"){
+                    div.style .backgroundImage = `url(${path}/White_Bishop.png)`;
+                }         
+                break;
+            }
+            case "knight": { // cheval
+                if(this.pieceColor === "black"){
+                    div.style.backgroundImage = `url(${path}/Black_Knight.png)`;
+                }else if(this.pieceColor === "white"){
+                    div.style .backgroundImage = `url(${path}/White_Knight.png)`;
+                }         
+                break;
+            }
+            case "Rook": { // tour
+                if(this.pieceColor === "black"){
+                    div.style.backgroundImage = `url(${path}/Black_Rook.png)`;
+                }else if(this.pieceColor === "white"){
+                    div.style .backgroundImage = `url(${path}/White_Rook.png)`;
+                }         
+                break;
+            }
+            case "pawn": { // pion
+                if(this.pieceColor === "black"){
+                    div.style.backgroundImage = `url(${path}/Black_Pawn.png)`;
+                }else if(this.pieceColor === "white"){
+                    div.style .backgroundImage = `url(${path}/White_Pawn.png)`;
+                }         
+                break;
+            }
+        }
+    }
+    ShowMove(){
+        console.log("showMove gradeb : ", this.name.name)
+        switch(this.name.name){
+            case "king": {
+                console.log("king !")
+                let move = King.ShowMove();
+                move.forEach(div => {
+                    console.log(div)
+                    div.style.backgroundColor = "red";
+                })
+            }
+            case "queen": {
+
+            }
+        }
+    }    
+}
+
+class King extends Grade{
+    constructor(){
+        this.name = "king";
+    }
+    ShowMove(x, y){
+        let possibleMoves = []; 
+        if(chessBoardArray[x-1]){ // check 1 ligne devant
+            if(DestionationColor !== User.color){
+                if(chessBoardArray[x-1][y-1]){
+                    possibleMoves.push(chessBoardArray[x-1][y-1]);
+                    chessBoardArray[x-1][y-1].htmlElement.style.backgroundColor = "red";
+                }
+                if(chessBoardArray[x-1][y]){
+                    possibleMoves.push(chessBoardArray[x-1][y]);
+                    chessBoardArray[x-1][y].htmlElement.style.backgroundColor = "red";
+                }
+                if(chessBoardArray[x-1][y+1]){
+                    possibleMoves.push(chessBoardArray[x-1][y+1]);
+                    chessBoardArray[x-1][y+1].htmlElement.style.backgroundColor = "red";
+                }
+            }
+        }
+        return possibleMoves;
+    }
+}
+
+// instanciation des grades
+let gradeKing = new Grade("king");
+
+// variables
 let chessBoardArray = [[], [], [], [], [], [], [], [], ];
 let activClick = false;
 let chessBoard = document.getElementById("chessBoard");
@@ -90,22 +189,22 @@ function createPlayBoard(){
             if(x % 2 === 0){
                 if(y % 2 !== 0){
                     newCase.createCase(line, "black");
-                    newCase.grade = "king";
-                }else{
-                    newCase.createCase(line, "white");           
+                }else{                                       
+                    newCase.createCase(line, "white");                   
                 }
                 console.log(lineArray)
                 chessBoardArray[x].push(newCase);
             }else{
-                if(y % 2 === 0){
+                if(y % 2 === 0){                    
                     newCase.createCase(line, "black");
-                }else{
-                    newCase.createCase(line, "white");           
+                }else{                       
+                    newCase.createCase(line, "white");             
                 }
                 chessBoardArray[x].push(newCase);
-            }
-            
+            }          
         }
     }
+    chessBoardArray[7][7].pieceColor = "black";
+    chessBoardArray[7][7].grade = gradeKing;
 }
 
